7378953a0b0e1a0c568ee3da8aeb4df3b474e0ef
Derek <physci@gmail.com>
2020-01-08 15:24:36 -0500

    remove some boiler plate with const functions.

--- src/dsp/colors.rs
+++ src/dsp/colors.rs
@@ -1,57 +1,28 @@
 use crate::dsp::types::*;
 use sdl2::pixels::Color as SdlColor;
 
-pub const WHITE: Color = Color {
-    r: 255,
-    g: 255,
-    b: 255,
-};
-pub const BLACK: Color = Color { r: 0, g: 0, b: 0 };
-pub const BLUE: Color = Color { r: 0, g: 0, b: 255 };
-pub const LIGHT_BLUE: Color = Color {
-    r: 200,
-    g: 200,
-    b: 255,
-};
-pub const GREEN: Color = Color { r: 0, g: 255, b: 0 };
-pub const RED: Color = Color { r: 255, g: 0, b: 0 };
-pub const GREY: Color = Color {
-    r: 50,
-    g: 50,
-    b: 50,
-};
+const fn c(r: u8, g: u8, b: u8) -> Color {
+    Color { r, g, b }
+}
 
-pub const GRAY: Color = Color {
-    r: 50,
-    g: 50,
-    b: 50,
-};
-
-pub const GRID_GRAY: Color = Color {
-    r: 0xEE,
-    g: 0xEE,
-    b: 0xEE,
-};
-
-pub const BACKGROUND: Color = Color {
-    r: 250,
-    g: 250,
-    b: 250,
-};
-
-pub const CURSOR_LIGHT: Color = Color {
-    r: 190,
-    g: 190,
-    b: 190,
-};
-pub const CURSOR_DARK: Color = Color {
-    r: 23,
-    g: 23,
-    b: 23,
-};
+pub const WHITE: Color = c(255, 255, 255);
+pub const BLACK: Color = c(0, 0, 0);
+pub const BLUE: Color = c(0, 0, 255);
+pub const LIGHT_BLUE: Color = c(200, 200, 255);
+pub const GREEN: Color = c(0, 255, 0);
+pub const RED: Color = c(255, 0, 0);
+pub const GREY: Color = c(50, 50, 5);
+pub const GRAY: Color = c(50, 50, 5);
+pub const GRID_GRAY: Color = c(0xEE, 0xEE, 0xEE);
+pub const BACKGROUND: Color = c(250, 250, 2);
+pub const CURSOR_LIGHT: Color = c(190, 190, 190);
+pub const CURSOR_DARK: Color = c(23, 23, 23);
 
 impl Color {
     pub fn as_sdl(&self) -> SdlColor {
         SdlColor::RGB(self.r, self.g, self.b)
     }
+    // pub fn as_gl(&self) -> SdlColor {
+    //     SdlColor::RGB(self.r, self.g, self.b)
+    // }
 }
