b2e0fd027ae26be16061593bb01b5fcea360258d
Derek <physci@gmail.com>
2020-01-12 14:11:28 -0500

    shader fiddling

--- src/dsp/vertex_shaders.rs
+++ src/dsp/vertex_shaders.rs
@@ -3,10 +3,11 @@ pub const VERTEX_SHADER_SOURCE: &str = r#"
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor; // Specify a vertex attribute for color
+
 out highp vec3 color;
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    gl_Position = vec4(aPos.x/4.0, aPos.y/4.0, aPos.z/4.0, 1.0);
 	color = aColor; // pass the color along to the fragment shader
 }
 
