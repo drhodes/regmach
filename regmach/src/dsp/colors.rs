f3e4c6cb78fab5ebc6e1a32bb45da217dc3974eb
Derek <physci@gmail.com>
2020-01-18 17:53:48 -0500

    sorts colors

--- regmach/src/dsp/colors.rs
+++ regmach/src/dsp/colors.rs
@@ -4,19 +4,19 @@ const fn c(r: u8, g: u8, b: u8) -> Color {
     Color { r, g, b }
 }
 
-pub const WHITE: Color = c(255, 255, 255);
+pub const BACKGROUND: Color = c(250, 250, 250);
 pub const BLACK: Color = c(0, 0, 0);
 pub const BLUE: Color = c(0, 0, 255);
-pub const LIGHT_BLUE: Color = c(200, 200, 255);
-pub const GREEN: Color = c(0, 255, 0);
-pub const RED: Color = c(255, 0, 0);
-pub const GREY: Color = c(50, 50, 5);
-pub const GRAY: Color = c(50, 50, 5);
-pub const GRID_GRAY: Color = c(0xEE, 0xEE, 0xEE);
-pub const BACKGROUND: Color = c(250, 250, 250);
-pub const CURSOR_LIGHT: Color = c(190, 190, 190);
 pub const CURSOR_DARK: Color = c(23, 23, 23);
+pub const CURSOR_LIGHT: Color = c(190, 190, 190);
+pub const GRAY: Color = c(50, 50, 5);
+pub const GREEN: Color = c(0, 255, 0);
+pub const GREY: Color = c(50, 50, 5);
+pub const GRID_GRAY: Color = c(0xEE, 0xEE, 0xEE);
 pub const JADE_BLUE: Color = c(38, 139, 210);
+pub const LIGHT_BLUE: Color = c(200, 200, 255);
+pub const RED: Color = c(255, 0, 0);
+pub const WHITE: Color = c(255, 255, 255);
 
 impl Color {
     pub fn as_gl(&self) -> (f32, f32, f32, f32) {
