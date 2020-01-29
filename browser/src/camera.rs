feaa686483287fdafb2d863f9b599cf67143da2b
Derek <physci@gmail.com>
2020-01-29 10:17:06 -0500

    cleanup and debugging mouse picking

--- browser/src/camera.rs
+++ browser/src/camera.rs
@@ -5,20 +5,20 @@ use nalgebra_glm as glm;
 // by thebennybox, https://www.youtube.com/watch?v=e3sc72ZDDpo
 impl Camera {
     pub fn new(pos: V3, fov: f32, aspect: f32, z_near: f32, z_far: f32) -> Camera {
-        Camera {
-            pos,
-            fov,
-            aspect,
-            z_near,
-            z_far,
-            perspective: glm::perspective(aspect, fov, z_near, z_far),
-            forward: V3::new(0.0, 0.0, 1.0),
-            up: V3::new(0.0, 1.0, 0.0),
-        }
+        Camera { pos,
+                 fov,
+                 aspect,
+                 z_near,
+                 z_far,
+                 perspective: glm::perspective(aspect, fov, z_near, z_far),
+                 forward: V3::new(0.0, 0.0, 1.0),
+                 up: V3::new(0.0, 1.0, 0.0) }
     }
 
     pub fn default() -> Camera {
-        Camera::new(V3::new(0f32, 0f32, -10f32), 3.14159 / 2.0, 1.0, 0.0, 1000.0)
+        let fov = std::f32::consts::PI / 2.0;
+        let z_near = 1.0; // if this is zero, then mouse picking will not work.
+        Camera::new(V3::new(0f32, 0f32, -10f32), fov, 1.0, z_near, 1000.0)
     }
 
     pub fn update_aspect(&mut self, w: f64, h: f64) {
@@ -37,7 +37,7 @@ impl Camera {
     }
 
     pub fn zoom_in(&mut self) {
-        if self.pos.z > -0.1 {
+        if self.pos.z > -self.z_near {
             return;
         }
         self.pos.z /= 1.125;
@@ -57,11 +57,11 @@ impl Camera {
     }
 
     pub fn pan_up(&mut self) {
-        self.pos.y -= self.zoom_factor();
+        self.pos.y += self.zoom_factor();
     }
 
     pub fn pan_down(&mut self) {
-        self.pos.y += self.zoom_factor();
+        self.pos.y -= self.zoom_factor();
     }
 
     pub fn center(&mut self) {
--- browser/src/start.rs
+++ browser/src/start.rs
@@ -16,27 +16,13 @@ pub fn main() -> Result<(), JsValue> {
     let mut dsp: BrowserDisplay = BrowserDisplay::new();
 
     let verts: Vec<f32> = vec![-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
-    let mut mesh = Mesh::from_verts(&dsp,
-                                    verts,
-                                    include_str!("../shaders/basic-shader.vs"),
-                                    include_str!("../shaders/basic-shader.fs"))?;
+    let mut triangle = Mesh::from_verts(&dsp,
+                                        verts,
+                                        include_str!("../shaders/basic-shader.vs"),
+                                        include_str!("../shaders/basic-shader.fs"))?;
 
     let grid = Grid::new(&dsp)?;
-
-    // let font_data = include_bytes!("../../media/font/routed-gothic.ttf");
-    // let collection = FontCollection::from_bytes(font_data as &[u8]).unwrap_or_else(|e| {
-    //                      panic!("error constructing a FontCollection from bytes: {}", e);
-    //                  });
-
-    // let font = collection.into_font() // only succeeds if collection consists of one font
-    //                      .unwrap_or_else(|e| {
-    //                          panic!("error turning FontCollection into a Font: {}", e);
-    //                      });
-
-    // let text = Text::new(&dsp, regmach::dsp::colors::JADE_BLUE, &font, "DRAM[63:0]")?;
-
     let mut texts = vec![];
-
     for i in 0..100 {
         texts.push(dsp.add_text(rdt::Command::AddText(i as f32,
                                                       i as f32,
@@ -50,48 +36,56 @@ pub fn main() -> Result<(), JsValue> {
     let f = Rc::new(RefCell::new(None));
     let g = f.clone();
 
-    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
-        dsp.update_canvas_size_todo();
-        dsp.props.frame_increment();
-        dsp.clear();
-        for ev in &dsp.get_events() {
-            // there should be an event driven way adjust the canvas size.
-            // dsp.watch_for_window_resize_awful();
-            match ev {
-                rdt::Event::MouseDown(p) => {
-                    log!("processing {:?}, vertex_buffer: {:?}", ev, mesh.vertex_buffer);
-                    let scmpoint = dsp.screen_to_schematic(p.x as u32, p.y as u32);
-                    log!("scmpoint: {:?}", scmpoint);
-                }
-                rdt::Event::MouseMove(p) => {
-                    //log!("processing {:?}", ev);
-                }
-                rdt::Event::KeyDown(code) => {
-                    log!("processing {:?}", ev);
-                    match *code {
-                        65 => dsp.camera.pan_left(),
-                        68 => dsp.camera.pan_right(),
-                        87 => dsp.camera.pan_up(),
-                        83 => dsp.camera.pan_down(),
-                        33 => dsp.camera.zoom_in(),
-                        34 => dsp.camera.zoom_out(),
-                        67 => dsp.camera.center(),
-                        _ => log!("unhandled key {:?}", ev),
-                    }
-                }
-                _ => {
-                    log!("unhandled event: {:?}", ev);
-                }
-            }
-        }
+    *g.borrow_mut() =
+        Some(Closure::wrap(Box::new(move || {
+                               // there should be an event driven way adjust the canvas size.
+                               dsp.update_canvas_size_todo();
+                               dsp.props.frame_increment();
+                               dsp.clear();
 
-        grid.draw(&dsp);
-        mesh.draw_with_mode(&dsp, GL::TRIANGLES);
-        text.draw_with_mode(&dsp, GL::TRIANGLES);
+                               for ev in &dsp.get_events() {
+                                   // there should be an event driven way adjust the canvas size.
+                                   match ev {
+                                       rdt::Event::MouseDown(p) => {
+                                           log!("processing {:?}, vertex_buffer: {:?}",
+                                                ev,
+                                                triangle.vertex_buffer);
+                                           let scmpoint = dsp.screen_to_schematic(p.x as u32, p.y as u32);
+                                           log!("scmpoint: {:?}", scmpoint);
+                                       }
+                                       rdt::Event::MouseMove(p) => {
+                                           let pos = dsp.screen_to_schematic(p.x as u32, p.y as u32);
+                                           triangle.move_to(pos.x, pos.y);
+                                       }
+                                       rdt::Event::KeyDown(code) => {
+                                           log!("processing {:?}", ev);
+                                           match *code {
+                                               68 => dsp.camera.pan_left(),
+                                               65 => dsp.camera.pan_right(),
+                                               87 => dsp.camera.pan_up(),
+                                               83 => dsp.camera.pan_down(),
+                                               33 => dsp.camera.zoom_in(),
+                                               34 => dsp.camera.zoom_out(),
+                                               67 => dsp.camera.center(),
+                                               _ => log!("unhandled key {:?}", ev),
+                                           }
+                                       }
+                                       _ => {
+                                           log!("unhandled event: {:?}", ev);
+                                       }
+                                   }
+                               }
 
-        // Schedule another requestAnimationFrame callback.
-        request_animation_frame(f.borrow().as_ref().unwrap());
-    }) as Box<dyn FnMut()>));
+                               grid.draw(&dsp);
+                               triangle.draw_with_mode(&dsp, GL::TRIANGLES);
+
+                               for text in texts.iter() {
+                                   text.draw_with_mode(&dsp, GL::TRIANGLES);
+                               }
+
+                               // Schedule another requestAnimationFrame callback.
+                               request_animation_frame(f.borrow().as_ref().unwrap());
+                           }) as Box<dyn FnMut()>));
 
     request_animation_frame(g.borrow().as_ref().unwrap());
     Ok(())
@@ -106,9 +100,8 @@ fn window() -> web_sys::Window {
 }
 
 fn request_animation_frame(f: &Closure<dyn FnMut()>) {
-    window()
-        .request_animation_frame(f.as_ref().unchecked_ref())
-        .expect("should register `requestAnimationFrame` OK");
+    window().request_animation_frame(f.as_ref().unchecked_ref())
+            .expect("should register `requestAnimationFrame` OK");
 }
 
 #[wasm_bindgen]
